81af5d7e942f696cc224287f4ad5e66956dce4e1
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2024-02-08 21:46:23 -0500

    Primer commit

---
+++ Point.java
@@ -0,0 +1,14 @@
+class Point{
+    private double x;
+    private double y;
+
+    public Point(){
+        System.out.println("Desde el contructor");
+        x = 0.0;
+        y = 0.0;
+    }
+
+    public void pointer(){
+        System.out.println("X equivale a: " + x + " Y equivale a: " + y +" así termina esto");
+    }
+}
